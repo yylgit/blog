9f15a1db1af1b9a15433483f8761533e09da35ad
yangyiliang <yangyiliang@didichuxing.com>
2017-08-19 12:33:03 +0800

    delete server

--- server/config/dev.js
+++
@@ -1,8 +0,0 @@
-var path = require('path')
-var defaultProjectRoot = path.resolve(__dirname,'../../../');
-
-module.exports = {
-  port: 8061,
-  assetDirAbsolute: path.resolve(__dirname, '../../dist'),
- 
-}
--- server/context/result.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (data, code, msg) {
-    this.body = {
-        code: code || 0,
-        msg: msg || '',
-        data: data || null,
-        isNodeServer: true
-    };
-}
