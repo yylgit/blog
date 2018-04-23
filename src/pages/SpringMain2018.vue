646a965d43e50c3f1c57f3f448e7ae66c8eae57f
yangyiliang <yangyiliang@didichuxing.com>
2018-04-23 14:33:25 +0800

    添加提示

--- src/pages/SpringMain2018.vue
+++ src/pages/SpringMain2018.vue
@@ -6,13 +6,15 @@
   <p>18届的同学们，是不是马上毕业手上还没有offer？是不是对现在的offer还不太满意？赶快来参加一下吧，赶紧上车，乘风破浪，2018注定是不平凡的一年！</p>
   <p>19届及以后的同学们，是不是想早点体验互联网公司的工作节奏？ 是不是想早点出来工作走上人生巅峰？赶快来实习吧，益处多多！</p>
   <p>网申分为普通网申和内部推荐，我是滴滴员工，把我的内部推荐链接分享给大家。详细介绍看下面的图片。</p>
+  <p class="red">仔细看下面说明，慎重选择，只能内推一个岗位</p>
+  <p class="red">选创建快捷简历，然后会给你的邮箱发送完善简历的邮件</p>
   <div class="item"  @click="gotoLink('SpringList2018')">点这里进入内部推荐</div>
   <p>通过我的内推链接投递的简历，可以通过yangyiliang@didichuxing.com邮箱联系我，查询志愿具体状态</p>
   <img src="../assets/2018Spring.jpeg" class="logo">
   </div>
 </template>
 
-<script></script>
+<script>
 export default {
   name: 'Main',
   data () {
@@ -57,6 +59,9 @@ export default {
     font-size: 18px;
     text-align: center;
     margin: 10px;
+  }
+  .red {
+    color: red;
   }
 }
 </style>
